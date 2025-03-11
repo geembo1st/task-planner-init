6b9d175f3bd93313c8f621afa883c945ee2e916e
Azat <kushkarov.azat@gmail.com>
2025-03-11 22:31:52 +0300

    first commit

---
+++ src/main/java/ru/azat/TaskPlannerInitService/TaskPlannerInitServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.azat.TaskPlannerInitService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskPlannerInitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskPlannerInitServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/azat/TaskPlannerInitService/TaskPlannerInitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.azat.TaskPlannerInitService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPlannerInitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
